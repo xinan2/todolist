a210ff472c3248e5f7b954ddc22a78245ad069da
Muhammad Salehin <xinan2@live.com>
2019-07-18 03:41:14 +0800

    v1

--- src/App.js
+++ src/App.js
@@ -1,26 +1,47 @@
-import React from 'react';
-import logo from './logo.svg';
+import React  from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class ToDoList extends React.PureComponent {
+  state = {
+    input: '',
+    itemList: [],
+  };
+
+  onInputChange = (e) => this.setState({input: e.target.value });
+
+  renderItemList = (item) => {
+    return this.state.itemList.map((item, index) => (
+        <div className="itemList">
+          <p>{item}</p>
+          <button onClick={() => this.removeItem(index)}>Remove</button>
+        </div>
+    ));
+  };
+
+  removeItem = (item) => this.setState({itemList: this.state.itemList.filter((list, index) => item !== index)});
+
+  addItem = (item) => this.setState({
+    itemList: this.state.itemList.concat(item),
+    input: '',
+  });
+
+  render() {
+    return (
+        <div className="App">
+          <div className="container">
+            <h2>To Do List</h2>
+            <h3>Add New To-Do</h3>
+            <input type="text" className="inputText" placeholder="Enter new task here" value={this.state.input} onChange={e => this.onInputChange(e)}/>
+            <div>
+              <button className="addButton" onClick={() => this.addItem(this.state.input)}>Add</button>
+            </div>
+            <div className="containerList">
+              {this.state.itemList && this.renderItemList(this.state.itemList)}
+            </div>
+          </div>
+        </div>
+    );
+  };
 }
 
-export default App;
+export default ToDoList;
