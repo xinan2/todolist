3db5cba4ef619fa603d582431c080c3efea1cd5b
Muhammad Salehin <xinan2@live.com>
2019-07-18 03:43:08 +0800

    Updates

---
+++ precache-manifest.01145908aa2b18d63c9f87d710d56426.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ff72117c9ecd5c442eca61c3eeb62650",
+    "url": "/todolist/index.html"
+  },
+  {
+    "revision": "8a7e71f75a81ddfa474f",
+    "url": "/todolist/static/css/main.767ea71f.chunk.css"
+  },
+  {
+    "revision": "681b51057d74cfa8975d",
+    "url": "/todolist/static/js/2.65aa1cca.chunk.js"
+  },
+  {
+    "revision": "8a7e71f75a81ddfa474f",
+    "url": "/todolist/static/js/main.1915699e.chunk.js"
+  },
+  {
+    "revision": "5225356577f332e6df46",
+    "url": "/todolist/static/js/runtime~main.d3bf418b.js"
+  }
+]);
